97d0b16c1efda0a58ff66891eeb6573c3a20ee4a
hitech95 <nicveronese@gmail.com>
2022-06-08 16:09:57 +0200

    feat: add recommendation endpoints
    Signed-off-by: hitech95 <nicveronese@gmail.com>

---
+++ src/model/recommend.ts
@@ -0,0 +1,67 @@
+import { AxiosInstance, AxiosResponse } from "axios";
+import { GorseException } from "../error";
+import { LatestOutput, PopularOptions, RecommendOptions } from "../interfaces";
+
+export function getPopular(
+  axios: AxiosInstance,
+  { category = "", cursorOptions }: PopularOptions
+) {
+  return axios
+    .get<string[], AxiosResponse<string[]>>(`/popular/${category}`, {
+      params: cursorOptions,
+    })
+    .then(({ data }) => {
+      return data;
+    })
+    .catch((exception) => {
+      const { response } = exception;
+      return Promise.reject(new GorseException(response.status, response.data));
+    });
+}
+
+export function getLatest(
+  axios: AxiosInstance,
+  { category = "", cursorOptions }: PopularOptions
+) {
+  return axios
+    .get<LatestOutput[], AxiosResponse<LatestOutput[]>>(`/latest/${category}`, {
+      params: cursorOptions,
+    })
+    .then(({ data }) => {
+      return data;
+    })
+    .catch((exception) => {
+      const { response } = exception;
+      return Promise.reject(new GorseException(response.status, response.data));
+    });
+}
+
+export function getRecommend(
+  axios: AxiosInstance,
+  {
+    userId,
+    category = "",
+    cursorOptions,
+    writeBackType,
+    writeBackDelay,
+  }: RecommendOptions
+) {
+  return axios
+    .get<string[], AxiosResponse<string[]>>(
+      `/recommend/${userId}/${category}`,
+      {
+        params: {
+          ...(writeBackType ? { "write-back-type": writeBackType } : {}),
+          ...(writeBackDelay ? { "write-back-delay": writeBackDelay } : {}),
+          ...cursorOptions,
+        },
+      }
+    )
+    .then(({ data }) => {
+      return data;
+    })
+    .catch((exception) => {
+      const { response } = exception;
+      return Promise.reject(new GorseException(response.status, response.data));
+    });
+}
